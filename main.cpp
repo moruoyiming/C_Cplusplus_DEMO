6d0b4a6ccf4d5891734511e1e2fd66a16b148ef7
jian <moruoyiming123@gmail.com>
2021-02-23 23:59:04 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    int i = 200;
+    double j = 3000;
+    long m = 4000;
+    printf("what is %d \n",i);
+    printf("what is %lf \n",j);
+    return 0;
+}
