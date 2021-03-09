0ff76f17131f8bc37c8ff1dec0a14283ad8c87dd
JianRuiLin <jmoruoyiming123@gmail.com>
2021-03-09 18:14:32 +0800

    枚举  文件操作

---
+++ 24.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * 读取文件的大小
+ * @return
+ */
+int main() {
+    char *filename = "/Users/jian/Documents/development/test.txt";
+    FILE *file = fopen(filename, "rb");
+    if (!file) {
+        printf("文件打开失败");
+        exit(0);
+    }
+    //给file指针赋值，挪动的记录信息
+    fseek(file, 0, SEEK_END);
+    //读取刚刚给file赋值的信息
+    long file_size = ftell(file);
+    printf("%s文件的字节大小是:%d \n",filename,file_size);
+    fclose(file);
+    return 0;
+}
--- T23.c
+++ T23.c
@@ -7,7 +7,7 @@
  * 文件拷贝操作
  * @return
  */
-int main() {
+int main23() {
 //   fopen开发文件 参数一 文件路径  参数二 操作类型   返回值 FILE 结构体
 //   File * fopen(const char *, const char *);
     char *filenamesrc = "/Users/jian/Documents/development/test.txt";
