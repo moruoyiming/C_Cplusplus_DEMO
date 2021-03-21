5426d693e2ccf58f43f1d9dcb54f734661f00b04
菅瑞林（北京） <ruilin.jian@chukong-inc.com>
2021-03-21 22:24:46 +0800

    可变参数

--- T40.cpp
+++ T40.cpp
@@ -2,11 +2,36 @@
 #include <stdarg.h>
 
 using namespace std;
+
+class Worker {
+public:
+    char *name;
+    int age = NULL;
+
+    Worker(){
+
+    }
+
+    Worker(char *name, int age){
+
+    }
+
+    /**
+     * 原理 为什么可以修改age
+     * 默认持有隐式this(Worker * const this 指针常量)
+     * const int * (指针常量代表指针地址不能修改，但是指针地址的值可以修改)
+     */
+    void change(){
+        this->age=78;
+    }
+
+};
+
 /**
  * const 修饰
  * @return
  */
-int main40() {
+int main() {
 
     cout << "dsfasdf" << endl;
     return 0;
